ed1f27f197c6cc6e438bb953b109b3f07c7dbddf
ywngj1989 <3124894812@qq.com>
2020-08-14 11:10:06 +0800

    unitest用法

--- testcase/test_calc.py
+++ testcase/test_calc.py
@@ -1,45 +1,44 @@
 from unittest import TestCase
+from ddt import ddt, data, file_data, unpack
 from src.calc import Calc
+import yaml
 
 
+@ddt
 class TestCalc(TestCase):
-    def test_add(self):
-        calc = Calc()
-        result = calc.add(1, 1)
-        #assert result == 2
-        self.assertEqual(result, 2)
 
-        result = calc.add(1, 0)
-        #assert result == 1
-        self.assertEqual(result, 1)
+    def setUp(self) -> None:
+        self.calc = Calc()
 
-        result = calc.add(1, -1)
-        #assert result == 0
-        self.assertEqual(result, 0)
+    @data(1, 5, 6)
+    def test_demo(self, real):
+        self.assertEqual(real, 5)
 
-        result = calc.add(1, 10000)
-        #assert result == 10001
-        self.assertEqual(result, 10001)
+    @data((1, 1, 2),
+          (1, 0, 1),
+          (1, -1, 0),
+          (1, 10000, 10001)
+          )
+    @unpack
+    def test_add(self, a, b, c):
+        result = self.calc.add(a, b)
+        self.assertEqual(result, c)
 
-    def test_div(self):
-        calc = Calc()
-        result = calc.div(2, 1)
-        self.assertEqual(2, result)
-
-        result = calc.div(1, 2)
-        self.assertEqual(result, 0.5)
-
-        result = calc.div(2, 0)
-        self.assertEqual(result, None)
+# 参数方在yaml文件中
+    @file_data("calc.yaml")
+    def test_div(self, a, b, c):
+        result = self.calc.div(a, b)
+        self.assertEqual(result, c)
 
     def test_above(self):
-        calc = Calc()
-
-        result = calc.above(1, 2)
+        result = self.calc.above(1, 2)
         self.assertEqual(result, False)
 
-        result = calc.above(2, 1)
+        result = self.calc.above(2, 1)
         self.assertEqual(result, True)
 
-        result = calc.above(1, 1)
+        result = self.calc.above(1, 1)
+        self.assertEqual(result, False)
+
+        result = self.calc.above(1.1, 2.1)
         self.assertEqual(result, False)
